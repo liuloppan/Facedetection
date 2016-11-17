884bba60bd6b3c272823e7f1dbba8bb940037798
liuloppan <lovha997@student.liu.se>
2016-11-18 00:53:52 +0800

    cleaned, commented, filtering with contour

--- FaceDetection/Main.cpp
+++ FaceDetection/Main.cpp
@@ -1,276 +1,123 @@
+/*
+Date: 17/11-2016
+Written by: Lovisa Hassler
+
+*****Functionality********
+This code segments out facial area in video of a person sitting in front of a computer.
+The technique used is filtering out relevant pixels based on color.
+Details of the skin color model that is used can be found in the paper
+'Face Segmentation Using Skin-Color Map in Videophone Applications' by Chai and Ngan.
+
+*****Limitations********
+A face will have a large cluster of skin coloured pixels while the background
+may or may not have large clusters of "skin colored" pixels.
+Three conditions have to apply for this code to be used successfully.
+1. Make sure you have proper lighting for the camera and the room and that the background 
+don't have too many objects that can be similar to skin in color.
+2. The face have to be the largest skin colored component in the image, this means there
+can't be too large clusters of skin colored background noise that can be interpreted as the face
+3. Light hair and other 'skin colored" objects can be classified as skin and will not be filtered out
+if they are somehow connected to the face.
+*/
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
-
 #include "opencv2/imgproc.hpp"
-//#include <algorithm>
 
 using namespace cv;
 using namespace std;
 
-//void FindLargestComponant(Mat&);
-void DensityFilter(const Mat& myImage, Mat& Result);
-void DensityErodeDilate(Mat& anImage, Mat& Result);
+
+void LargestContourFilter(Mat& anImage, Mat& Result);
 
 int main(int argc, char** argv)
 {
+	Mat image; // original image
 
-	if (argc != 2)
+	//video handling
+	VideoCapture videoCap; //open camera, 0 is default
+	videoCap.open(0);
+	videoCap >> image; //read first frame
+
+	//check to see if video worked
+	if (!image.data) // Check for invalid input
 	{
-		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+		cout << "Could not open or find video input" << std::endl;
 		return -1;
 	}
-	//some different image matrices to follow the 
-	Mat image;
-	Mat result;
-	Mat imYCrCb;
-	Mat density;
 
-	// YCrCb range for skin model as recommended by professor 
+	//some different image matrices to follow the process
+	Mat imYCrCb; //filtered image in YCrCb color space
+	Mat skinBinIm(image.rows, image.cols, CV_8UC1); //binary image containging all the potential skinpixels
+	Mat contourim;
+
+	// YCrCb range for skin model
 	Scalar minYCrCb(0, 133, 77); //min values of skin model in YCrBc color space, originally (0, 133, 77)
 	Scalar maxYCrCb(255, 173, 127); // max values of skin model in YCrBc color space, originally (0, 173, 127)
 
-
-	//VideoCapture videoCap; //open camera, 0 is default
-	//videoCap.open(0);
-
-	image = imread(argv[1], IMREAD_COLOR); // Read the file in BGR color (BGR is same as RGB but other order)
-	//videoCap >> image;
-	
-	if (!image.data) // Check for invalid input
-	{
-		cout << "Could not open or find the image" << std::endl;
-		return -1;
-	}
-
-
+	//Some windows to show input and output
 	namedWindow("Original", WINDOW_AUTOSIZE); // Create a window for display.
 	namedWindow("Result", WINDOW_AUTOSIZE); // Create a window for display.
-	namedWindow("FinalImage", WINDOW_AUTOSIZE); // Create a window for display.
-
-	imshow("Original", image); // Show our image inside it.
 
 	//***HERE IS WHERE THE MAGIC HAPPENS***//
 
-	//bgr 2 YCrCb of image
-	cvtColor(image, imYCrCb, CV_BGR2YCrCb);
+	while (1) //videoloop
+	{
+		videoCap >> image; //read next frame from camera
+		imshow("Original", image);
 
-	//Segment to CV_8U type bitmap, white (255) if pixel is within skinmodel, black (0) otherwise
-	inRange(imYCrCb, minYCrCb, maxYCrCb, result); //result is a 1 channel mat containging type CV_8U
+		//bgr 2 YCrCb of image
+		cvtColor(image, imYCrCb, CV_BGR2YCrCb);
 
-	Mat dst;
-	//Density map handling to get rid of noise
-	DensityFilter(result, density); //function that filters iamge into density filter image values
-	DensityErodeDilate(density, dst); //function that handles the density values according to neihbouring pixels
+		//Segment to CV_8U type bitmap, white (255) if pixel is within skinmodel, black (0) otherwise
+		inRange(imYCrCb, minYCrCb, maxYCrCb, skinBinIm); //result is a 1 channel mat containging type CV_8U
 
+		//to get rid of all unimportant background noise we want the largest component
+		//To get rid of them we use the function LargestContourFilter
+		//This function will find the largest contours and draw it filled
+		LargestContourFilter(skinBinIm, contourim);
 
-	//make connected component map. A face will have a large cluster of skin coloured pixels while the background 
-	//may or may not have large clusters of skincolored pixels. Find connected components
-	
-	Mat ccStats(image.size(), CV_32SC1);
-	Mat ccCentroids(image.size(), CV_32SC1);
-	Mat labelimage(image.size(), CV_32SC1);
+		//lastly we cope all relevant skin pixels from the original image
+		//by using contourim as a mask
+		Mat result; //generate new result matrice everytime
+		image.copyTo(result, contourim);
 
-	int nlabels = connectedComponentsWithStats(result, labelimage, ccStats, ccCentroids, 8, CV_32S);
-	int componentArea = 0;
-	int maxComponentArea = -1;
-	int maxLabel = -1;
+		imshow("Result", result); // display result
 
-	//cout << ccStats.rows << ccStats.cols;// << ccStats.channels();
+		if (waitKey(30) >= 0) break; //break for escape button
+									 //waitKey(33); //run forever
+	} // end of videoloop
 
-	//get the largest connected component to filter out background noise
-
-	for (int labelcount = 1; labelcount < nlabels; ++labelcount) { // skip 0 which is background
-		componentArea = ccStats.at<int>(labelcount, CC_STAT_AREA);
-
-		if (componentArea >= maxComponentArea) 
-		{
-			maxComponentArea = componentArea;
-			maxLabel = labelcount;
-			cout << "maxlabel " << maxLabel << "maxcomponentarea "<< maxComponentArea;
-		}
-	}
-
-	//loop through to find max area labels
-	Mat maxComp(image.size(), CV_8UC1);
-	for (int r = 0; r < image.rows; ++r) {
-		for (int c = 0; c < image.cols; ++c) {
-			int label = labelimage.at<int>(r, c);
-
-			if (label == maxLabel) {
-				uchar &pixel = maxComp.at<uchar>(r, c);
-				pixel = 255.0f;
-
-			}
-			else {
-				uchar &pixel = maxComp.at<uchar>(r, c);
-				pixel = 0.0f;
-			}
-		}
-	}
-
-	//imshow("Result", result); // display result
-	Mat finalim(image.size(), CV_8UC3);
-
-
-	//for (int r = 0; r < image.rows; ++r) {
-	//	for (int c = 0; c < image.cols; ++c) {
-	//		Vec3b pixel = maxComp.at<Vec3b>(r, c);
-
-	//		if (pixel != Vec3b(0,0,0)) {
-	//			Vec3b &pixel = finalim.at<Vec3b>(r, c);
-	//			pixel = image.at<Vec3b>(r, c);
-
-	//		}
-	//		else {
-	//			Vec3b &pixel = finalim.at<Vec3b>(r, c);
-	//			pixel = colors[0];
-	//		}
-	//	}
-	//}
-	imshow("Result", density); // display result
-
-	imshow("Process", dst); // display result
-	imshow("FinalImage", maxComp); // display result
-
-	waitKey(0); // Wait for a keystroke in the window
 	return 0;
 }
 
+//Function accepts only 8-bit 1 channel images
+//Generates a 8 bit binary result image that shows the largest contour filled with white
+void LargestContourFilter(Mat& anImage, Mat& Result) {
 
-//void filter to density values.
-//0 value is defined as highly unlikely being a skin pixel, but depends on surrounding pixels
-//0.5 value might be skin pixel, depending on its surroundings
-//1.0 is likely to be skin pixel, but can be set to zero if few surrounding pixels are white
-void DensityFilter(const Mat& myImage, Mat& Result)
-{
-	CV_Assert(myImage.depth() == CV_8U);  // accept only uchar images
-	Mat anImage;
-	myImage.convertTo(anImage, CV_32FC1);
-	Result.create(myImage.size(), CV_32FC1);
-	//const int nChannels = myImage.channels();
+	CV_Assert(anImage.depth() == CV_8U);   // accept only uchar images
 
-	//first, assign pixelvalues according to neighbouring averages
-	for (int j = 2; j < myImage.rows - 2; ++j)
+	Result = Mat(anImage.rows, anImage.cols, CV_8UC1, Scalar::all(0));
+	vector<vector<Point>> contours; //vector of the contours of the binary image
+	vector<Vec4i> hierarchy; //hierarchies of the contours
+
+	findContours(anImage, contours, hierarchy, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE); //only get external contours
+
+	int largest_area = 0;
+	int largest_contour_index = 0;
+
+	for (int i = 0; i< contours.size(); i++) // iterate through each contour to find the largest
 	{
-		//create some pointers to the rows for looping
-		const float* previous2 = anImage.ptr<float>(j - 2);
-		const float* previous = anImage.ptr<float>(j - 1);
-		const float* current = anImage.ptr<float>(j);
-		const float* next = anImage.ptr<float>(j + 1);
-		const float* next2 = anImage.ptr<float>(j + 2);
-
-		float* output = Result.ptr<float>(j);
-
-		for (int i = 2; i < (myImage.cols - 2); ++i)
+		double a = contourArea(contours[i], false);  //  Find the area of contour
+		if (a>largest_area)
 		{
-			float val = (current[i]+ current[i - 1] + current[i + 1] + current[i - 2] + current[i + 2] + 
-				previous[i] + previous[i-1] + previous[i + 1] + previous[i - 2] + previous[i + 2] +
-				next[i] + next[i - 1] + next[i + 1] + next[i - 2] + next[i + 2] +
-				previous2[i] + previous2[i - 1] + previous2[i + 1] + previous2[i - 2] + previous2[i + 2]+
-				next2[i] + next2[i - 1] + next2[i + 1] + next2[i - 2] + next2[i + 2] );
-			//cout << static_cast<float>(val);
-			if (val == 0) //if pixel and all 5x5 neighbours are black
-				*output++ = 0.0f;
-			else if(val >= (255 * 25)) //if pixel and all 5x5 neighbours are white
-				*output++ = 1.0f;
-			else //if pixel and its 5x5 neighbours are a mix of black and white
-				*output++ = 0.5f;
+			largest_area = a;
+			largest_contour_index = i;                //Store the index of largest contour
 		}
 	}
-
-	//set the edges to black
-	Result.row(0).setTo(Scalar(0));
-	Result.row(1).setTo(Scalar(0));
-	Result.row(Result.rows - 1).setTo(Scalar(0));
-	Result.row(Result.rows - 2).setTo(Scalar(0));
-	Result.col(0).setTo(Scalar(0));
-	Result.col(1).setTo(Scalar(0));
-	Result.col(Result.cols - 1).setTo(Scalar(0));
-	Result.col(Result.cols - 2).setTo(Scalar(0));
+	Scalar color(255, 255, 255);
+	drawContours(Result, contours, largest_contour_index, color, CV_FILLED, 8, hierarchy); //draw an fill largest contour
 }
-
-void DensityErodeDilate(Mat& anImage, Mat& Result) {
-
-	CV_Assert(anImage.depth() == CV_32F);  // accept only float images
-	Result.create(anImage.size(), CV_32FC1);
-
-	for (int j = 1; j < anImage.rows - 1; ++j)
-	{
-		//create some pointers to the rows for looping
-		const float* previous = anImage.ptr<float>(j - 1);
-		const float* current = anImage.ptr<float>(j);
-		const float* next = anImage.ptr<float>(j + 1);
-
-		float* output = Result.ptr<float>(j);
-
-
-
-		for (int i = 1; i < (anImage.cols - 1); ++i)
-		{
-			int skincount = 0;  //reset skincount
-
-			//calculate how many neighbours are white density values aka high probability skin pixels
-			for (int r = (j-1); r <= (j+1); ++r) {
-				const float* rowPtr = anImage.ptr<float>(r);
-				for (int c = (i - 1); c <= (i + 1); ++c){
-					if (rowPtr[c] == 1.0f)
-					skincount++; //count the number of neighbouring white density values
-				}
-			}
-
-
-			if (current[i] == 1.0f) { //set to 0.0f if surrounded by less than 5 other 1.0f values in 3x3 neighbourhood
-
-				if (skincount < 5) //if less than five white neighours
-					*output++ = 0.0f;
-				else //if pixel and its 3x3 neighbours have 5 or more high density values
-					*output++ = 1.0f;
-
-			}
-			else {  //if 0.5 or 0.0 set to 1.0f if surrounded by more than two 1.0 (high density)values in 3x3 neighbourhood
-				
-				if (skincount >= 2) //if two or more neighbouring high density vlues
-					*output++ = 1.0f;
-				else //if pixel and its 3x3 neighbours have less than two high density values
-					*output++ = 0.0f;
-			}
-
-		}
-	}
-
-}
-
-//int main(int, char**) {
-	//
-	//	Mat frame;
-	//	Mat result;
-	//
-	//	VideoCapture videoCap; //open camera, 0 is default
-	//	videoCap.open(0);
-	//	if (!videoCap.isOpened()) { //check if it works
-	//		cout << "Could not open or find the video" << std::endl;
-	//		return -1;
-	//	}
-	//
-	//	namedWindow("Original video", WINDOW_AUTOSIZE); // Create a window for display.
-	//	namedWindow("Result", WINDOW_AUTOSIZE); // Create a window for display.
-	//	while (1)
-	//	{
-	//
-	//		videoCap >> frame;
-	//
-	//		imshow("Original video", frame);
-	//		cvtColor(frame, result, COLOR_BGR2GRAY);
-	//		//GaussianBlur(result, result, Size(7, 7), 1.5, 1.5);
-	//		Canny(result, result, 0, 30, 3);
-	//		imshow("Result", result);
-	//
-	//		if (waitKey(30) >= 0) break; //break for escape button
-	//									 //waitKey(33); //run forever
-	//	}
-	//	//camera deinitialized automatically in videocapture destructor
-	//	return 0;
-	//};
